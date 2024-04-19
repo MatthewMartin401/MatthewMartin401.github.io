cc3376ec2de12c80ac97325df728a3bbb94a4bb9
Matt <129974886+MatthewMartin401@users.noreply.github.com>
2024-04-19 21:13:37 +0100

    Update
    Fixed JSON references.
    Added Video support for projects pages.
    Started replacing Main pages project list with a grid that displays a grid of buttons containing an image of displaying the project.

---
+++ LoadProject.js
@@ -0,0 +1,128 @@
+import data from "./ProjectData.json" with {type : "json"};
+
+const info = data
+console.log(data)
+
+//  Gets info from URL Parameters
+const URL_Param = new URLSearchParams(window.location.search)
+const project = URL_Param.get("project")
+// console.log(project)
+
+// Tab Title
+const t = document.getElementById("title")
+t.innerHTML = info[project]["name"]
+
+// Page Title
+document.getElementById("project-title").innerHTML = info[project]["name"]
+
+// Dependancy Info
+const dis_libaries = document.getElementById("libraries-used")
+console.log(">")
+for(let i = 0; i < info[project]["libraries"].length; i++){
+    // console.log(i)
+    // let new_item = document.createElement("li").innerHTML = info[project]["libraries"][i]
+    // dis_libaries.append(new_item)
+    let li = document.createElement("li")
+    li.textContent = info[project]["libraries"][i]
+    console.log(li.textContent)
+    dis_libaries.appendChild(li)
+}
+
+// Selected Item
+    // Image
+const selectedImage = document.getElementById("imgSelected")
+
+    // Video
+const selectedVideo = document.getElementById("vidSelected")
+// const selectedVid = document.getElementById("vidSelected")
+
+const ChangeImage = (x) => {
+        // Change to image.
+    if (x["src"].toLowerCase().endsWith("png")){
+        console.log("img")
+        selectedImage.src = x.src
+        selectedImage.alt = x.alt
+
+        selectedImage.id = "imgSelected"
+        selectedVideo.id = "vidSelected_hid"
+    }
+        // Change to video.
+    else if(x["src"].toLowerCase().startsWith("http")){
+        console.log("Vid")
+        selectedVideo.src = x.src
+        selectedVideo.alt = x.alt
+        // selectedVideo.width = 420
+        // selectedVideo.height = 315
+        console.log(selectedImage.hidden)
+        
+
+        selectedImage.id = "imgSelected_hid"
+        selectedVideo.id = "vidSelected"
+    }
+}
+
+    // Hides Image
+if(info[project]["img"][0]["src"].toLowerCase().endsWith("png")){
+        // Image
+    selectedImage.id = "imgSelected"
+    selectedImage.src = info[project]["img"][0]["src"]
+
+        // Video
+    selectedVideo.id = "vidSelected_hid"
+}
+
+    // Hides Video
+else if(info[project]["img"][0]["src"].toLowerCase().startsWith("https")){
+        // Image
+    selectedImage.id = "imgSelected_hid"
+
+        // Video
+    selectedVideo.id = "vidSelected"
+    selectedVideo.src = info[project]["img"][0]["src"]
+}
+
+// Image Gallery
+const gallery = document.getElementById("gallery")
+for(let i = 0; i < info[project]["img"].length; i++){
+    // console.log(info[project]["img"][i]["src"])
+
+    // Create sub image 
+    if(info[project]["img"][i]["src"].toLowerCase().endsWith("png")){
+        // console.log(true)
+        let img = document.createElement("img")
+        img.onclick = () => {
+            // console.log(img)
+            ChangeImage(img)
+        }
+        img.alt = info[project]["img"][i]["alt"]
+        img.src = info[project]["img"][i]["src"]
+        img.className = "project-img"
+        gallery.appendChild(img)
+    }
+
+    // Create sub iframe
+    else if(info[project]["img"][i]["src"].toLowerCase().startsWith("https")){
+        console.log("Video Loaded")
+        let vidImg = document.createElement("img")
+        let vid = document.createElement("img")
+        vid.src = info[project]["img"][i]["src"]
+        vid.alt = info[project]["img"][i]["src"]
+        vidImg.onclick = () => {
+            ChangeImage(vid)
+        }
+        vidImg.alt = info[project]["img"][0]["alt"]
+        vidImg.src = info[project]["img"][0]["src"]
+        vidImg.className = "project-img"
+        gallery.appendChild(vidImg)
+
+        // let vid = document.createElement("video")
+        // vid.onclick = () =>{
+        //     ChangeImage(vid)
+        // }
+        // vid.alt = info[project]["img"][i]["alt"]
+        // vid.src = info[project]["img"][i]["src"]
+        // gallery.appendChild(vid)
+    }
+}
+
+document.getElementById("description").innerHTML = info[project]["des"]
--- index.js
+++ index.js
@@ -1,8 +1,64 @@
-// const root = document.createElement("root")
-// root.appendChild(document.createElement("html"))
-// // root.
-// root = HTMLFormElement
-// console.log("hi")
+import data from "./ProjectData.json" with {type : "json"};
+    
+const Projects = data["projects-list"]
+console.log(Projects)
 
-const h = document.createElement("HTML")
-console.log("hi")
+const project_table = document.getElementById("Project_list")
+
+for(let x = 0; x < Projects.length; x++){
+    let item = Projects[x]
+    // console.log(x)
+    // console.log(item)
+    // console.log(item.length)
+    if(item[1].length > 0){
+        let lan = document.createElement("li")
+        // console.log(item[0])
+        lan.textContent = item[0]
+        project_table.appendChild(lan)
+
+        let container = document.createElement("ul")
+        project_table.appendChild(container)
+        for(let i = 1; i < item.length; i++){
+            let li = document.createElement("li")
+            let url = document.createElement("a")
+            // console.log(`${item[1]}`)
+            url.innerText = item[i][0]
+            url.href = `Project.html?project=${item[i][1]}`
+            container.appendChild(li)
+            li.appendChild(url)
+        }
+        // project_table.appendChild(li)
+        // return
+        }
+    else{
+        continue
+        console.log("Not projects in this language")
+    }
+
+    // console.log(Object.keys(data))
+    // console.log(Object.values(data))
+    // console.log(Object.keys(data).length)
+    for(let i = 1; i < Projects.length; i++){
+        if(Projects[x].length > 0){
+            console.log(Object.keys(data)[i])
+            console.log(i)
+            let img = document.createElement("img")
+            // console.log(data[Object.keys(data)[i]])
+            console.log(data[Object.keys(data)[i]]["alt"])
+            img.src = data[Object.keys(data)[i]]["img"][0]["src"]
+            img.alt = data[Object.keys(data)[i]]["img"][0]["alt"]
+            img.innerHTML = Object.keys(data)[i]
+
+            
+
+            let button = document.createElement("button")
+            button.appendChild(img)
+
+            let a = document.createElement("a")
+            a.href = `Project.html?project=${Object.keys(data)[i]}`
+            a.appendChild(button)
+
+            document.getElementById("ProjectListButtons").appendChild(a)
+        }
+    }
+}
